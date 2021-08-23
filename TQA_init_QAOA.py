621ab3052904108827ec30312d2c59d95c8ad59a
jvillanueva <jedwvv@gmail.com>
2021-08-23 14:27:47 +1000

    commit

--- TQA_init_QAOA.py
+++ TQA_init_QAOA.py
@@ -1,4 +1,5 @@
 from time import time
+import sys
 import warnings
 import numpy as np
 import pickle as pkl
@@ -64,15 +65,15 @@ def main(args = None):
         initial_state, mixer = (None, None)
 
     fourier_parametrise = args["fourier"]
-    print("-"*50)
-    print("Now solving with TQA_QAOA... Fourier Parametrisation: {}".format(fourier_parametrise))
+    print_to_file("-"*50)
+    print_to_file("Now solving with TQA_QAOA... Fourier Parametrisation: {}".format(fourier_parametrise))
     for p in range(1, p_max+1):
         construct_circ = False
         deltas = np.arange(0.45, 0.91, 0.05)
         point = np.append( [ (i+1)/p for i in range(p) ] , [ 1-(i+1)/p for i in range(p) ] )
         points = [delta*point for delta in deltas]
-        print("-"*50)
-        print("    "+"p={}".format(p))
+        print_to_file("-"*50)
+        print_to_file("    "+"p={}".format(p))
         if fourier_parametrise:
             points = [ convert_to_fourier_point(point, len(point)) for point in points ]
         qaoa_results, optimal_circ = CustomQAOA(operator,
@@ -89,7 +90,7 @@ def main(args = None):
         exp_val = qaoa_results.eigenvalue * max_coeff
         state_solutions = { item[0][::-1]: item[1:] for item in qaoa_results.eigenstate }
         for item in sorted(state_solutions.items(), key = lambda x: x[1][1], reverse = True)[0:5]:
-            print( item )
+            print_to_file( item )
         prob_s = 0
         for string in x_s:
             prob_s += state_solutions[string][1] if string in state_solutions else 0
@@ -98,27 +99,32 @@ def main(args = None):
         if fourier_parametrise:
             optimal_point = convert_from_fourier_point(optimal_point, len(optimal_point))
         approx_ratio = 1 - np.abs( (opt_value - exp_val) / opt_value )
-        print("    "+"Optimal_point: {}".format(optimal_point))
-        print("    "+"Exp_val: {}, Prob_s: {}, approx_ratio: {}".format(exp_val, prob_s, approx_ratio))
+        print_to_file("    "+"Optimal_point: {}".format(optimal_point))
+        print_to_file("    "+"Exp_val: {}, Prob_s: {}, approx_ratio: {}".format(exp_val, prob_s, approx_ratio))
         approx_ratios.append(approx_ratio)
         prob_s_s.append(prob_s)
-    print("-"*50)
-    print("QAOA terminated")
-    print("-"*50)
-    print("Approximation ratios per layer", approx_ratios)
-    print("Prob_success per layer", prob_s_s)
+    print_to_file("-"*50)
+    print_to_file("QAOA terminated")
+    print_to_file("-"*50)
+    print_to_file("Approximation ratios per layer", approx_ratios)
+    print_to_file("Prob_success per layer", prob_s_s)
     save_results = np.append(approx_ratios, prob_s_s)
     if fourier_parametrise:
         with open('results_{}cars{}routes/TQA_F_{}.csv'.format(args["no_cars"], args["no_routes"], args["no_samples"]), 'w') as f:
             np.savetxt(f, save_results, delimiter=',')
-        print("Results saved in results_{}cars{}routes/TQA_F_{}.csv".format(args["no_cars"], args["no_routes"], args["no_samples"]))
+        print_to_file("Results saved in results_{}cars{}routes/TQA_F_{}.csv".format(args["no_cars"], args["no_routes"], args["no_samples"]))
     else:
         with open('results_{}cars{}routes/TQA_NF_{}.csv'.format(args["no_cars"], args["no_routes"], args["no_samples"]), 'w') as f:
             np.savetxt(f, save_results, delimiter=',')
-        print("Results saved in results_{}cars{}routes/TQA_NF_{}.csv".format(args["no_cars"], args["no_routes"], args["no_samples"]))
+        print_to_file("Results saved in results_{}cars{}routes/TQA_NF_{}.csv".format(args["no_cars"], args["no_routes"], args["no_samples"]))
     finish = time()
-    print("Time Taken: {}".format(finish - start))
+    print_to_file("Time Taken: {}".format(finish - start))
 
+filepath = "TQA_output.txt"
+def print_to_file(string, filepath = filepath):
+    print(string)
+    with open(filepath, 'a') as f:
+        print(string, file=f)
 
 if __name__ == "__main__":
     main()
